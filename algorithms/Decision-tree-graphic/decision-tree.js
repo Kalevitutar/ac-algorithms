85c476cb166943d77040530008c0650eb4bed0a1
Kalevitutar <tanyamthomas@yahoo.com>
2023-10-31 17:00:11 -0400

    decision tree partly done

--- algorithms/Decision-tree-graphic/decision-tree.js
+++ algorithms/Decision-tree-graphic/decision-tree.js
@@ -1,3 +1,27 @@
 function transportationOptions() {
-    // Write code here to make the decision tree into an algorithm!
+  // Write code here to make the decision tree into an algorithm!
+
+  let weather;
+  let time;
+  let transportation;
+  let w = "walk";
+  let b = "bus";
+  let hungry;
+
+  if (weather === "sun") {
+    if (time > 30) {
+      transportation = w;
+    } else {
+      transportation = b;
+    }
+  } else if (weather === "cloud") {
+    hungry ? (transportation = w) : b;
+  } else if (weather === "rain") {
+    transportation = b;
+  } else {
+    console.log(
+      "Oh, if you'd just hoofed it instead of bothering about all this, you'd be there already, plus you know you don't have bus fare anyway"
+    );
   }
+  console.log(transportation);
+}
